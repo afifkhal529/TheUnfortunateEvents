f26aceb00efcf07a6248150a9e43b917d1a47c11
Afifa Khalid <afifkhal529@ed.amdsb.ca>
2022-03-28 14:53:05 -0400

    Add project files.

---
+++ TheUnfortunateEvents/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace TheUnfortunateEvents
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.option3Button = new System.Windows.Forms.Button();
+            this.option1Label = new System.Windows.Forms.Label();
+            this.option2Label = new System.Windows.Forms.Label();
+            this.option3Label = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Snow;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(49, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(696, 119);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "label1";
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // option1Button
+            // 
+            this.option1Button.Location = new System.Drawing.Point(49, 370);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(59, 53);
+            this.option1Button.TabIndex = 1;
+            this.option1Button.Text = "button1";
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // option2Button
+            // 
+            this.option2Button.Location = new System.Drawing.Point(283, 370);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(59, 53);
+            this.option2Button.TabIndex = 2;
+            this.option2Button.Text = "button2";
+            this.option2Button.UseVisualStyleBackColor = true;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // option3Button
+            // 
+            this.option3Button.Location = new System.Drawing.Point(517, 370);
+            this.option3Button.Name = "option3Button";
+            this.option3Button.Size = new System.Drawing.Size(59, 53);
+            this.option3Button.TabIndex = 3;
+            this.option3Button.Text = "button3";
+            this.option3Button.UseVisualStyleBackColor = true;
+            this.option3Button.Click += new System.EventHandler(this.option3Button_Click);
+            // 
+            // option1Label
+            // 
+            this.option1Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.option1Label.Location = new System.Drawing.Point(114, 370);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(163, 53);
+            this.option1Label.TabIndex = 4;
+            this.option1Label.Text = "label1";
+            this.option1Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // option2Label
+            // 
+            this.option2Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.option2Label.Location = new System.Drawing.Point(348, 370);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(163, 53);
+            this.option2Label.TabIndex = 5;
+            this.option2Label.Text = "label2";
+            this.option2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // option3Label
+            // 
+            this.option3Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.option3Label.Location = new System.Drawing.Point(582, 370);
+            this.option3Label.Name = "option3Label";
+            this.option3Label.Size = new System.Drawing.Size(163, 53);
+            this.option3Label.TabIndex = 6;
+            this.option3Label.Text = "label3";
+            this.option3Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(186, 131);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(415, 211);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSalmon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.option3Label);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Label);
+            this.Controls.Add(this.option3Button);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.outputLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "form1";
+            this.Text = "The Unfortunate Events";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Button option3Button;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.Label option3Label;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TheUnfortunateEvents/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace TheUnfortunateEvents
+{
+    public partial class form1 : Form
+    {
+        // Tracks what page of the story the user is at
+        int page = 1;
+        Random randGen = new Random();
+        int random;
+
+        public form1()
+        {
+            InitializeComponent();
+            DisplayPage();
+
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 3;
+            }
+
+            else if (page == 6)
+            {
+                random = randGen.Next(1, 5);
+                if (random < 2)
+                {
+                    page = 9;
+                }
+                else
+                {
+                    page = 10;
+                }
+            }
+            else if (page == 11)
+            {
+                page = 13;
+            }
+            else if (page == 5)
+            {
+                page = 12;
+            }
+            else if (page == 15)
+            {
+                page = 16;
+            }
+            else if (page == 16)
+            {
+                page = 18;
+            }
+            else if (page == 18)
+            {
+                page = 20;
+            }
+            else if (page == 20)
+            {
+                page = 22;
+            }
+
+
+            DisplayPage();
+        }
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 6;
+            }
+            else if (page == 6)
+            {
+                page = 7;
+            }
+            else if (page == 2)
+            {
+                page = 8;
+            }
+            else if (page == 11)
+            {
+                page = 14;
+            }
+            else if (page == 5)
+            {
+                page = 15;
+            }
+            else if (page == 15)
+            {
+                page = 17;
+            }
+            else if (page == 16)
+            {
+                page = 19;
+            }
+            else if (page == 18)
+            {
+                page = 21;
+            }
+            else if (page == 20)
+            {
+                page = 23;
+            }
+
+
+            DisplayPage();
+
+        }
+
+        private void option3Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 4;
+            }
+
+        }
+        public void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = $"You are enjoying your evening stroll in the streets of Toronto. Cool air rushes against your face as you admire the beautiful evening sky, and the luxuries that the earth has given.";
+                    outputLabel.Text += $"\n";
+                    outputLabel.Text += $"You continue to stroll down the streets that you loose track of time. Soon you come to realize that you have walked to the other side of the city and it's nearly dark!";
+                    
+                    outputLabel.Text += $"\nAfraid of the night, you want to reach your home as quickly as you can.";
+                    outputLabel.Text += $"\n";
+                    outputLabel.Text += $"\nYou can either call a taxi, walk, or ask someone for a lift. What do you choose?";
+
+                    option1Label.Text = $"Call a taxi";
+                    option2Label.Text = $"Ask someone for a lift";
+                    option3Label.Text = $"Walk";
+                    break;
+                case 2:
+                    outputLabel.Text = $"You call a taxi to get home safe and sound. You continuously glance at your phione during the taxi ride, just to make sure that the driver does not take a wrong turn";
+                    outputLabel.Text += $"\n";
+                    outputLabel.Text += $"\nTHUD! The car violently jerked. You and the driver are alarmed at the sudden incident. The alarmned driver steps out of the car to inspect the cause. After 5 minutes the driver steps back inside the car and informs you that the car hit a pothole, which caused the front right tire to puncture.";
+                    //Refresh();
+                    //Thread.Sleep(5000);
+                    outputLabel.Text += $"\nYou can either wait for a mechanic to come fix the car or walk to your home all by yourself";
+                    option1Label.Text = $"Wait for the mechanic";
+                    option2Label.Text = $"Walk back home";
+                    option3Label.Text = $"";
+                    break;
+                case 3:
+                    outputLabel.Text = $"The mechanic arrives after 30 minutes, fixes the car and you arrive home safely.";
+                    outputLabel.Text += $"\n";
+                    outputLabel.Text += $"\n Play Again?";
+
+                    option1Label.Text = $"Yes";
+                    option2Label.Text = $"No";
+                    option3Label.Text = $"";
+                    break;
+                case 4:
+                    outputLabel.Text = $"You choose to walk back home. After what feels like 30 minutes, you are startled by a hight pitched scream. You frantically look around to see where the screen came from. Then, from the corner of your eye, you spot 2 persons across the street.";
+                    outputLabel.Text += $"";
+                    outputLabel.Text += $"";
+
+                    option1Label.Text = $"";
+                    option2Label.Text = $"";
+                    option3Label.Text = $"";
+                    break;
+                case 5:
+                    outputLabel.Text = $"";
+                    outputLabel.Text += $"";
+                    outputLabel.Text += $"";
+
+                    option1Label.Text = $"";
+                    option2Label.Text = $"";
+                    option3Label.Text = $"";
+                    break;
+                case 6:
+                    outputLabel.Text = $"You wave down the first car you see and ask for a lift. After riding for awhile, they start to slow down. You start to realize that your about to be abducted.";
+                    
+                    outputLabel.Text += $"\n";
+                    outputLabel.Text += $"You can either panic or call the police secretly";
+
+                    option1Label.Text = $"Call police";
+                    option2Label.Text = $"Panic";
+                    option3Label.Text = $"";
+                    break;
+                case 7:
+                    outputLabel.Text = $"You get taken away never to be seen again. You later die of loneliness.";
+                    outputLabel.Text += $"\n";
+                    outputLabel.Text += $"\nPlay Again?";
+
+                    option1Label.Text = $"Yes";
+                    option2Label.Text = $"No";
+                    option3Label.Text = $"";
+                    break;
+                case 8:
+                    outputLabel.Text = $"You get hit by a truck and die";
+                    outputLabel.Text += $"\n";
+                    outputLabel.Text += $"\nPlay Again";
+
+                    option1Label.Text = $"";
+                    option2Label.Text = $"";
+                    option3Label.Text = $"";
+                    break;
+                case 9:
+                    outputLabel.Text = $"The police catch the car and arrest the driver, while you safely live your life happily";
+                    outputLabel.Text += $"\n";
+                    outputLabel.Text += $"Play Again?";
+
+                    option1Label.Text = $"Yes";
+                    option2Label.Text = $"No";
+                    option3Label.Text = $"";
+                    break;
+                case 10:
+                    outputLabel.Text = $"You are not found ever again.";
+                    outputLabel.Text += $"\n";
+                    outputLabel.Text += $"Play Again?";
+
+                    option1Label.Text = $"Yes";
+                    option2Label.Text = $"No";
+                    option3Label.Text = $"";
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                case 14:
+                    break;
+                default:
+                    break;
+
+
+            }
+        }
+
+    }
+}
